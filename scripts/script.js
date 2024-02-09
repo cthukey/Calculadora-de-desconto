00553f96f20a50081c3e33cc789c8856c15d9f0f
Pedro Henrique <cezaogamer@gmail.com>
2024-02-09 11:03:24 -0300

    Updated now

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ let inputCampo2 = document.querySelector('#campo2')
 let myDesconto = document.querySelector('#DesEmReais')
 let total = document.querySelector('#res')
 let myForm = document.querySelector('form')
+let myRecarregarPagina = document.querySelector('#novoCalculo')
 
 
 myForm.addEventListener('change',function(){
@@ -11,13 +12,17 @@ myForm.addEventListener('change',function(){
 
     let campo1 = Number(inputCampo1.value)
     let campo2 = Number(inputCampo2.value)
-    console.log(total)
 
-    let theDeconto = `${campo1 * campo2/100}`
-    myDesconto.innerHTML = `Desconto em reais: ${theDeconto}` 
+    let theDesconto = `${campo1 * campo2/100}`
+    myDesconto.innerHTML = `Desconto em reais: ${theDesconto}` 
 
-    let thePrecoFinal = `${campo1 - theDeconto}`
+
+    let thePrecoFinal = `${campo1 - theDesconto}`
     total.innerHTML =`R$ ${thePrecoFinal}`
 
+})
+
+myRecarregarPagina.addEventListener('click',function(){
+    window.location.reload()
 })
 
